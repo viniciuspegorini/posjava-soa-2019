146ddebb7458e13863dceb46013eaf153cdfda95
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-05-11 15:45:27 -0300

    commit 5

--- posjava3-auth/src/main/java/br/edu/utfpr/pb/posjava3auth/Posjava3AuthApplication.java
+++ posjava3-auth/src/main/java/br/edu/utfpr/pb/posjava3auth/Posjava3AuthApplication.java
@@ -3,6 +3,9 @@ package br.edu.utfpr.pb.posjava3auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import br.edu.utfpr.pb.posjava2common.security.JwtConfig;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +13,11 @@ public class Posjava3AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Posjava3AuthApplication.class, args);
+	}
+	
+	@Bean
+	public JwtConfig jwtConfig() {
+		return new JwtConfig();
 	}
 	
 }
--- posjava3-auth/src/main/java/br/edu/utfpr/pb/posjava3auth/security/SecurityConfig.java
+++ posjava3-auth/src/main/java/br/edu/utfpr/pb/posjava3auth/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,8 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
 
 import br.edu.utfpr.pb.posjava2common.security.JwtConfig;
 
@@ -36,19 +35,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 	
 		http.csrf().disable()
-			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.exceptionHandling()
-				.authenticationEntryPoint((req, rsp, e) -> 
-				rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-			.and()
-			.addFilter( 
-					new JwtUsernameAndPasswordAuthenticationFilter(
-					authenticationManager(), jwtConfig))
-			.authorizeRequests()
-			.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
-			.anyRequest().authenticated();
+		.sessionManagement().sessionCreationPolicy(
+				SessionCreationPolicy.STATELESS).and()
+		.exceptionHandling()
+		.authenticationEntryPoint((req, rsp, e) 
+				-> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+		.and()
+		.addFilter(
+				new JwtUsernameAndPasswordAuthenticationFilter(
+						authenticationManager(), jwtConfig))
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+		.anyRequest().authenticated();
 			
 	}
 	
